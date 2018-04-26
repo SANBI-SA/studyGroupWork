4b13996a5a2670688fb94b0a9d68abaf173f998e
pvanheus <pvh@webbedfeet.co.za>
2018-04-26 11:46:53 +0200

    Update with suggestions from study group discussion

--- session15/separ_dna_seq_into_files.py
+++ session15/separ_dna_seq_into_files.py
@@ -6,12 +6,13 @@
 ######################################################################################################
 
 import os
+import os.path
 
 ## create one folder per bin
-for bin_lower in range(100, 1000, 100):  # creates bins that go from  100 - 1000, in increments of 100. These are the start no's
-	bin_upper = bin_lower + 99  # the upper limit will just be the bin + 99, so the first bin would be 100 - 199
-	bin_folder_name = str(bin_lower) + "_" + str(bin_upper) # name of folder would be each bin (eg. 100_199)
-	os.mkdir(bin_folder_name) #create a bunch of directories all in one go
+# for bin_lower in range(100, 1000, 100):  # creates bins that go from  100 - 1000, in increments of 100. These are the start no's
+# 	bin_upper = bin_lower + 99  # the upper limit will just be the bin + 99, so the first bin would be 100 - 199
+# 	bin_folder_name = str(bin_lower) + "_" + str(bin_upper) # name of folder would be each bin (eg. 100_199)
+# 	os.mkdir(bin_folder_name) #create a bunch of directories all in one go
 
 # create a variable to hold the sequence number
 seq_number = 1
@@ -35,12 +36,14 @@ for file_name in os.listdir("."): #returns a list of files and folders. It takes
 				if length >= bin_lower and length < bin_upper: #cannot be = to upper_bin, because upper bin, starts new directory
 					print ("bin is " + str(bin_lower) + " to " + str(bin_upper)) # once we know the correct bin, write out the sequence
 					bin_folder_name = str(bin_lower) + "_" + str(bin_upper)
+					if not os.path.exists(output_path):
+						os.mkdir(output_path)
 					output_path = bin_folder_name + "/" + str(seq_number) + ".dna"
 					output = open(output_path, "w")
-					output.write(dna)
+					output.write(dna + '\n')
 					output.close()
 					seq_number = seq_number + 1
-					
+					break
 				
 		
 			
