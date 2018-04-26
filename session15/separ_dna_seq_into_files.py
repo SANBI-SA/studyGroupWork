67c1a82db0b1698cc5d7c36d4a6900ad81265c6f
Peter van Heusden <pvh@sanbi.ac.za>
2018-04-26 15:06:13 +0200

    Fixed indentation

--- session15/separ_dna_seq_into_files.py
+++ session15/separ_dna_seq_into_files.py
@@ -1,14 +1,14 @@
 ######################################################################################################
 # Binning DNA sequences
-# Write a program which creates nine new folders – one for sequences between 100 and 199 bases long, 
-# one for sequences between 200 and 299 bases long, etc. 
+# Write a program which creates nine new folders – one for sequences between 100 and 199 bases long,
+# one for sequences between 200 and 299 bases long, etc.
 # Write out each DNA sequence in the input files to a separate file in the appropriate folder.
 ######################################################################################################
 
 import os
 import os.path
 
-## create one folder per bin
+# create one folder per bin
 # for bin_lower in range(100, 1000, 100):  # creates bins that go from  100 - 1000, in increments of 100. These are the start no's
 # 	bin_upper = bin_lower + 99  # the upper limit will just be the bin + 99, so the first bin would be 100 - 199
 # 	bin_folder_name = str(bin_lower) + "_" + str(bin_upper) # name of folder would be each bin (eg. 100_199)
@@ -18,33 +18,29 @@ import os.path
 seq_number = 1
 
 # process all files that end in .dna
-for file_name in os.listdir("."): #returns a list of files and folders. It takes a single argument which is a string containing the path of the folder whose contents you want to search. To get a list of the contents of the current working directory, 
+for file_name in os.listdir("."): #returns a list of files and folders. It takes a single argument which is a string containing the path of the folder whose contents you want to search. To get a list of the contents of the current working directory,
 #use the string "." for the path
-	#print (file_name)
-	if file_name.endswith(".dna"):
-		dna_file = open(file_name)
-		
-# for each line, calculate the sequence length
-		for line in dna_file:
-			dna = line.strip()
-			#print (dna)
-			length = len(dna)
-			print ("sequence length is " + str(length))
+#print (file_name)
+    if file_name.endswith(".dna"):
+        dna_file = open(file_name)
 
-			for bin_lower in range(100, 1000, 100):  # creates bins that go from  100 - 1000, in increments of 100. These are the start num	
-				bin_upper = bin_lower + 99
-				if length >= bin_lower and length < bin_upper: #cannot be = to upper_bin, because upper bin, starts new directory
-					print ("bin is " + str(bin_lower) + " to " + str(bin_upper)) # once we know the correct bin, write out the sequence
-					bin_folder_name = str(bin_lower) + "_" + str(bin_upper)
-					if not os.path.exists(bin_folder_name):
-						os.mkdir(bin_folder_name)
-					output_path = bin_folder_name + "/" + str(seq_number) + ".dna"
-					output = open(output_path, "w")
-					output.write(dna + '\n')
-					output.close()
-					seq_number = seq_number + 1
-					break
-				
-		
-			
+        # for each line, calculate the sequence length
+        for line in dna_file:
+            dna = line.strip()
+            #print (dna)
+            length = len(dna)
+            print ("sequence length is " + str(length))
 
+            for bin_lower in range(100, 1000, 100):  # creates bins that go from  100 - 1000, in increments of 100. These are the start num
+            bin_upper = bin_lower + 99
+            if length >= bin_lower and length < bin_upper: #cannot be = to upper_bin, because upper bin, starts new directory
+            print("bin is " + str(bin_lower) + " to " + str(bin_upper)) # once we know the correct bin, write out the sequence
+            bin_folder_name = str(bin_lower) + "_" + str(bin_upper)
+            if not os.path.exists(bin_folder_name):
+                os.mkdir(bin_folder_name)
+            output_path = bin_folder_name + "/" + str(seq_number) + ".dna"
+            output = open(output_path, "w")
+            output.write(dna + '\n')
+            output.close()
+            seq_number = seq_number + 1
+            break
--- session15/separ_dna_seq_into_files_with_function2.py
+++ session15/separ_dna_seq_into_files_with_function2.py
@@ -1,48 +1,48 @@
 ######################################################################################################
 # Binning DNA sequences
-# Write a program which creates nine new folders – one for sequences between 100 and 199 bases long, 
-# one for sequences between 200 and 299 bases long, etc. 
+# Write a program which creates nine new folders – one for sequences between 100 and 199 bases long,
+# one for sequences between 200 and 299 bases long, etc.
 # Write out each DNA sequence in the input files to a separate file in the appropriate folder.
 ######################################################################################################
 
 import os
 import os.path
 
+
 def find_bin(seqlength):
-  # we can predict the bin by looking at the number in the hundreds column of sequence length
-  # so e.g. 763 -> bin 700_799, 243 -> bin 200_299
-  # this number can be extracted by doing an integer division (the // operator) by 100
-  bin_number = seqlength // 100 
-  if bin_number < 1 or bin_number > 9:
-    raise ValueError("No appropriate bin was found for sequence of length {}".format(seqlength))
+    # we can predict the bin by looking at the number in the hundreds column of sequence length
+    # so e.g. 763 -> bin 700_799, 243 -> bin 200_299
+    # this number can be extracted by doing an integer division (the // operator) by 100
+    bin_number = seqlength // 100
+    if bin_number < 1 or bin_number > 9:
+        raise ValueError(
+            "No appropriate bin was found for sequence of length {}".format(seqlength))
 
-# create a variable to hold the sequence number
-seq_number = 1
+    # create a variable to hold the sequence number
+    seq_number = 1
 
-# process all files that end in .dna
-for file_name in os.listdir("."): #returns a list of files and folders. It takes a single argument which is a string containing the path of the folder whose contents you want to search. To get a list of the contents of the current working directory, 
-#use the string "." for the path
-	#print (file_name)
-	if file_name.endswith(".dna"):
-		dna_file = open(file_name)
-		
-# for each line, calculate the sequence length
-		for line in dna_file:
-			dna = line.strip()
-			#print (dna)
-			length = len(dna)
-			print ("sequence length is " + str(length))
+    # process all files that end in .dna
+    for file_name in os.listdir("."):  # returns a list of files and folders. It takes a single argument which is a string containing the path of the folder whose contents you want to search. To get a list of the contents of the current working directory,
+        # use the string "." for the path
+        #print (file_name)
+        if file_name.endswith(".dna"):
+            dna_file = open(file_name)
 
-      bin_folder_name = find_bin(length)
-      print ("bin is", bin_folder_name) # once we know the correct bin, write out the sequence
-      if not os.path.exists(bin_folder_name):
-        os.mkdir(bin_folder_name)
-      output_path = bin_folder_name + "/" + str(seq_number) + ".dna"
-      output = open(output_path, "w")
-      output.write(dna + '\n')
-      output.close()
-      seq_number = seq_number + 1
-      break
-				
-		
-			
+            # for each line, calculate the sequence length
+            for line in dna_file:
+                dna = line.strip()
+                #print (dna)
+                length = len(dna)
+                print("sequence length is " + str(length))
+
+                bin_folder_name = find_bin(length)
+                # once we know the correct bin, write out the sequence
+                print("bin is", bin_folder_name)
+                if not os.path.exists(bin_folder_name):
+                    os.mkdir(bin_folder_name)
+                output_path = bin_folder_name + "/" + str(seq_number) + ".dna"
+                output = open(output_path, "w")
+                output.write(dna + '\n')
+                output.close()
+                seq_number = seq_number + 1
+                break
